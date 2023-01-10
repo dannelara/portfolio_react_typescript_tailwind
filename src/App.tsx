49241771f59d7eedadaf4ed797e643834b82b421
Daniel <11daniel@live.se>
2023-01-10 16:26:51 +0100

    top

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import GlobalState from "./global/GlobalState";
 function App() {
   return (
     <GlobalState>
-      <div className="h-screen w-full relative bg-black flex flex-col" id="top">
+      <div className="h-screen w-full relative bg-black flex flex-col">
         <Header />
         <Main />
       </div>
